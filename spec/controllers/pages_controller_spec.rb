4c39f9b8903678030c3fe6d1421f98a1397a1041
Goffin <simon.goffin@outlook.com>
2013-09-12 22:57:43 +0200

    Fini avec les pages statiques

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,26 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
-    it "returns http success" do
+    it "devrait réussir" do
       get 'home'
       response.should be_success
     end
   end
 
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "devrait réussir" do
       get 'contact'
       response.should be_success
     end
   end
 
+  describe "GET 'about'" do
+    it "devrait réussir" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
